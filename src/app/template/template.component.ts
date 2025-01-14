d77fafa8c89d0ba4dd7773d63706c1a9cef83f75
BrigittePadilla <martha.padilla@croydon.com.co>
2025-01-14 13:21:32 -0500

    Creacion del formulario

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template.component.html',
   styleUrl: './template.component.css'
 })
 export class TemplateComponent {
+
+  persona = {
+    nombre:'',
+    edad:''
+  }
+
+  procesar(){
+    console.log(this.persona);
+  }
 
 }
