92db706909937f3e5b89723de5d899b93f6ffc99
BrigittePadilla <martha.padilla@croydon.com.co>
2025-01-14 10:03:55 -0500

    Instalacion de bootstrap y creacion de componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemplateComponent } from "./template/template.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TemplateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'formularioTemplateAngular';
+  title = 'Formulario Template o de Plantilla';
 }
